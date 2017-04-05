8cfd2cc99bbdcde2a736f71bf545611c84100be9
Nick Golding <goldingn35@gmail.com>
2017-04-05 12:16:10 +1000

    buildignore travis script and import zoon to clear a could of checks

--- R/zoontutorials.R
+++ R/zoontutorials.R
@@ -1,4 +1,5 @@
 #' Tutorials and Best Practice Guides for Species Distribution Modelling using zoon
 #' @name zoontutorials
 #' @description See vignette for an introductory tutorial to SDM using zoon
+#' @import zoon
 NULL
