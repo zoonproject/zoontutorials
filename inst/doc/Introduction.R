a235d9f75e3290d1a70d7ebcb4c6b82bc8860598
Nick Golding <goldingn35@gmail.com>
2017-06-06 10:55:03 +1000

    build vignettes

--- inst/doc/Introduction.R
+++ inst/doc/Introduction.R
@@ -6,18 +6,26 @@ knitr::opts_chunk$set(message = FALSE,
                dev = c('png'),
                cache = TRUE)
 
-## ----eval=TRUE, message=FALSE, warning=FALSE, fig.align='center', fig.height=4, fig.width=7, cache=TRUE----
-Dual_Model_Workflow <- workflow(occurrence = SpOcc("Thryothorus ludovicianus", 
-                                                   extent = c(-138.71, -52.58, 18.15, 54.95)),
-                                covariate = Bioclim(extent = c(-138.71, -52.58, 18.15, 54.95)),
-                                process = Background(1000),
-                                model = list(LogisticRegression, MaxEnt),
-                                output = PrintMap(points = FALSE))
+## ----echo = FALSE, out.width= 700, fig.cap="*Figure 1. Species Distribution Model Theory. Presence-background points plotted on geographic space, on environmental space, and probability of occurrence predictions plotted on environmental and geographic space.*", fig.align = "left"----
+knitr::include_graphics("../vignettes/Images/SDM_theory.png")
 
-## ----eval = FALSE--------------------------------------------------------
-#  save(Dual_Model_Workflow,
-#       file = 'Dual_Model_Workflow.RData')
+## ----message=FALSE, warning=FALSE, cache=TRUE----------------------------
+library(zoon)
 
-## ----eval = FALSE--------------------------------------------------------
-#  load('Dual_Model_Workflow.RData')
+## ----message=FALSE, warning=FALSE, fig.align='center', fig.height=7, fig.width=7----
+zoon_workflow <- workflow(occurrence = CarolinaWrenPO,
+                          covariate = CarolinaWrenRasters,
+                          process = Background(100),
+                          model = MaxEnt,
+                          output = InteractiveMap)
+
+## ----echo=FALSE, fig.align='center', fig.height=7, fig.width=7-----------
+# force the htmlwidget to render in the vignette
+Output(zoon_workflow)
+
+## ----eval=TRUE, message=FALSE, warning=FALSE, cache=TRUE-----------------
+head(Occurrence(zoon_workflow))
+
+## ----eval=TRUE, message=FALSE, warning=FALSE, cache=TRUE-----------------
+Covariate(zoon_workflow)
 
