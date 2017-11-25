21047c9f9e1c4e04159b470646c4f210b91f47a6
Doi90 <davidpw@student.unimelb.edu.au>
2017-11-26 00:00:51 +1100

    Data Exploration/Selecting Covariates copy edits for Saras sections + update mine. Rebuild vignettes

--- inst/doc/Selecting_Covariates.R
+++ inst/doc/Selecting_Covariates.R
@@ -7,8 +7,30 @@ knitr::opts_chunk$set(message = FALSE,
                cache = TRUE, 
                comment = '')
 
-## ----echo = F, out.width= '400px', fig.align = "center", fig.cap="*Figure 1. Conceptual flowchart for process modules in covariates selection and reparation.*"----
-knitr::include_graphics("../vignettes/Images/SelectCov_ConceptualDiagram.png")
+## ----clean workflow, eval = FALSE----------------------------------------
+#  Carolina_Wren_Workflow <- workflow(occurrence = CarolinaWrenPO,
+#                                     covariate = CarolinaWrenRasters,
+#                                     process = Chain(Background(1000),
+#                                                     Clean(which = c(1,2,3))),
+#                                     model = NullModel,
+#                                     output = NoOutput)
+
+## ----StandardiseCov, warning=FALSE, message=FALSE------------------------
+workflow_standardised <- workflow(CarolinaWrenPO,
+                                  CarolinaWrenRasters,
+                                  Chain(Background(1000),
+                                        StandardiseCov),
+                                  NullModel,
+                                  NoOutput)
+
+## ------------------------------------------------------------------------
+summary(Covariate(workflow_standardised))
+
+## ------------------------------------------------------------------------
+summary(Process(workflow_standardised)$ras)
+
+## ------------------------------------------------------------------------
+summary(scale(Covariate(workflow_standardised)))
 
 ## ---- eval=FALSE---------------------------------------------------------
 #  process = Transform(trans = function(x) {x^2},
@@ -27,28 +49,8 @@ knitr::include_graphics("../vignettes/Images/SelectCov_ConceptualDiagram.png")
 #                  Transform(trans = function(x) {log(x)},
 #                            which_cov = c("VarC", "VarD")))
 
-## ----Interaction_AllPairs, eval=FALSE------------------------------------
-#  process = addInteraction(which_covs = 'pairs')
-
-## ----Interaction_Pair, eval=FALSE----------------------------------------
-#  process = addInteraction(which_covs = c("A", "B"))   # adds an interaction between A & B
-
-## ----Interaction_MultPairs, eval=FALSE-----------------------------------
-#  process = addInteraction(which_covs = list(c("A","B"), c("A","C")))   # adds interactions between A & B and A & C, but not B & C
-
-## ----Interaction_Three-way, eval=FALSE-----------------------------------
-#  process = addInteraction(which_covs = c('A', 'B', 'C'))   # adds all two-way (e.g. A & B) interactions and a three-way interaction between A, B & C
-
 ## ----eval=FALSE----------------------------------------------------------
 #  process = addInteraction(which_covs = c('A', 'A'))   # leads to a quadratic polynomial
 #  
 #  process = addInteraction(which_covs = c('A', 'A', 'A'))   # leads to a cubic, polynomial
-
-## ----workflow, message = FALSE, warning = FALSE--------------------------
-# ext <- c(-170, -20, 10, 80)
-# SugarMaple_Workflow <- workflow(occurrence = SugarMaple,
-#                                 covariate = Bioclim(extent = as.vector(ext)),
-#                                 process = addInteraction(which_covs = c('Bio3', 'Bio3')),
-#                                 model = NullModel,
-#                                 output = ResponsePlot)
 
